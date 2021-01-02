64570217e17de6b13d6f4a41ce8a9d0ac585468f
farhadnowzari <fdn.sharp@gmail.com>
2021-01-02 14:45:48 +0100

    First deployment

---
+++ precache-manifest.920bf22bbaedfd36907ac1834c1dfacb.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9c4fe32210622fd898a",
+    "url": "/css/chunk-vendors.7dff8466.css"
+  },
+  {
+    "revision": "0d8bb39e00ee00f2a3fb2f51f791ef30",
+    "url": "/img/1.0d8bb39e.jpg"
+  },
+  {
+    "revision": "dd05f17b545b5f3b8f3c3a0b6f85e431",
+    "url": "/index.html"
+  },
+  {
+    "revision": "54b7fbd6731c976df469",
+    "url": "/js/app.76a5a6b1.js"
+  },
+  {
+    "revision": "e9c4fe32210622fd898a",
+    "url": "/js/chunk-vendors.2337b57f.js"
+  },
+  {
+    "revision": "bae63df35a125847700d5f34d0fd7e61",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "af3a090f8541adb0514d13c7a95457b9",
+    "url": "/media/1.af3a090f.mp3"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
